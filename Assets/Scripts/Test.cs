61cf516c59cdf118d569d7062874819fd9846e06
Equidona <betoprogrammer@gmail.com>
2024-02-06 22:13:38 -0600

    Siu

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,6 +7,24 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
+        Debug.Log("Hola");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {   
         
     }
 
